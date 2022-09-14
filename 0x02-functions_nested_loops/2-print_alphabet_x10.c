d1bb3254be9aff3fb1fdf0c6324e326e2a9b47d7
diamondpinn <diamondpinxy@gmail.com>
2022-09-13 20:37:15 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,24 +1,25 @@
-
 #include "main.h"
 /**
- *
- ** print_alphabet_x10 - function to print abc 10 times
- *
- **
- * Return: 0
- *
- **/
+*
+* print_alphabet_x10 - function to print abc 10 times
+*
+* Return: 0
+*
+**/
 
 void print_alphabet_x10(void)
 
 {
 
-       char c, i
+	char c;
+	int i;
 
        for (i = 0; i <= 9; i++)
-{ _putchar (c);
-}
-
-_putchar ['\n');
-
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
 }

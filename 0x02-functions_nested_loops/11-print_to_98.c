c5c5f417f274855a7fbc07d90fd9dba47dd154a0
diamondpinn <diamondpinxy@gmail.com>
2022-09-14 13:05:02 -0700

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,7 +4,7 @@
 /** print_to_98 - Prints all natural numbers from imput to 98,
  *  		  in order seperated by a comma followed by a space.
  *  @n:	The number to begin counting at.
- 
+ */ 
  	print_to_98(int n)
 {
 	{
